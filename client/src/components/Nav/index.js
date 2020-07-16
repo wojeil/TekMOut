a98f19051139b106967cf3c9e4b647df207c48bc
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-07-16 12:24:18 -0400

    nav component startup

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+class Nav extends React.Component {
+    constructor(props) {
+		super(props);
+		this.about = React.createRef();
+		this.projects = React.createRef();
+		this.contact = React.createRef();
+
+		this.scrolling = this.scrolling.bind(this);
+	}
+    render() {
+        return (
+        );
+    }
+}
